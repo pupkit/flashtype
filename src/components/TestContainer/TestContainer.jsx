5c5fd6320213f9b94ba4d3c54fef91cab09a8c7a
austin <austinsaganm@gmail.com>
2022-10-01 16:46:50 -0400

    Again

--- src/components/TestContainer/TestContainer.jsx
+++ src/components/TestContainer/TestContainer.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './TestContainer.css';
 import TryAgain from '../TryAgain/TryAgain';
+import TypingChallengeContainer from '../TypingChallengeContainer/TypingChallengeContainer';
 
 export default function TestContainer({ words, characters, wpm }) {
     return (
         <div className='test-container'>
             <div data-aos='fade-up' className='typing-challenge-cont'>
-                <h1>This is the test</h1>
+                <TypingChallengeContainer
+                    words={words}
+                    characters={characters}
+                    wpm={wpm}
+                />
             </div>
             {/* <div className='try-again-container'>
                 <TryAgain words={words} characters={characters} wpm={wpm} />
