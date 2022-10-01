9f2c09cfa647c38382f4414d0fa0ac22c9728830
austin <austinsaganm@gmail.com>
2022-09-30 20:02:54 -0400

    Navigation Bar

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Nav from '../Nav/Nav';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='app'>
+                {/*Nav Section*/}
+                <Nav />
+                {/*Landing Page*/}
+                {/*Challenge Section*/}
+                {/*Footer*/}
+            </div>
+        );
+    }
+}
+
+export default App;
