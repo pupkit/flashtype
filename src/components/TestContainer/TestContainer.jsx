ad26d7727a5309ed4f2865471ca10c28fa3171eb
austin <austinsaganm@gmail.com>
2022-10-01 15:33:48 -0400

    TestContainer

--- src/components/TestContainer/TestContainer.jsx
+++ src/components/TestContainer/TestContainer.jsx
@@ -5,9 +5,12 @@ import TryAgain from '../TryAgain/TryAgain';
 export default function TestContainer({ words, characters, wpm }) {
     return (
         <div className='test-container'>
-            <div className='try-again-container'>
-                <TryAgain words={words} characters={characters} wpm={wpm} />
+            <div data-aos='fade-up' className='typing-challenge-cont'>
+                <h1>This is the test</h1>
             </div>
+            {/* <div className='try-again-container'>
+                <TryAgain words={words} characters={characters} wpm={wpm} />
+            </div> */}
         </div>
     );
 }
