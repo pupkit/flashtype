38626fa95a2a1f0e92d342d1f45392da9bd9a64c
austin <austinsaganm@gmail.com>
2022-10-01 12:02:44 -0400

    Challenge Section Added

---
+++ src/components/ChallengeSection/ChallengeSection.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './ChallengeSection.css';
+
+export class ChallengeSection extends Component {
+    render() {
+        return (
+            <div className='challenge-section-container'>
+                <h1 data-aos='fade-down' className='challenge-section-header'>
+                    Take a speed test now!
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default ChallengeSection;
